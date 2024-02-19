59fc02edb745ac58fd725b738f826b67522bdf4b
rtyui01132001 <rtyui01132001@gmail.com>
2024-02-19 09:14:19 +0800

    新增交易紀錄、更新登入註冊頁

--- src/pages/HistoryPage.vue
+++ src/pages/HistoryPage.vue
@@ -1,11 +1,311 @@
 <template>
-  <q-page class="flex flex-center text-h1"> 交易紀錄 </q-page>
+  <q-page padding>
+    <div class="row fit row justify-between">
+      <div class="col-12">
+        <div class="BlockContent q-pr-lg">
+          <h5 class="mainTitle">查詢交易活動</h5>
+          <q-input v-model="text" label="依用戶名稱或電子郵件搜尋" bottom-slots rounded outlined>
+            <template v-slot:prepend><q-icon name="search" /></template>
+            <template v-slot:append><q-icon v-if="text !== ''" name="close" @click="text = ''"
+                class="cursor-pointer" /></template>
+            <template v-slot:after>
+              <q-btn color="warning" text-color="black" label="下載月結單" size="lg" class="q-px-xl" rounded />
+            </template>
+          </q-input>
+          <div class="filterBlock q-gutter-md">
+            <h5>篩選</h5>
+            <q-btn color="warning" label="全部" size="lg" class="q-px-xl" rounded unelevated outline />
+
+            <q-btn-dropdown color="warning" class="q-px-xl" label="日期區間" size="lg" rounded outline>
+              <div class="row no-wrap q-pa-md">
+                <div class="col-12">
+                  <div class="text-h6 q-mb-sm">日期</div>
+                  <q-option-group :options="dateOptions" type="radio" v-model="dateGroup" />
+                  <q-separator class="q-my-lg" />
+                  <div class="text-h6 q-mb-sm">日期區間</div>
+                  <div class="DateRange">
+                    <q-input filled v-model="dateStart" mask="date" :rules="['date']" class="DateInput">
+                      <template v-slot:append>
+                        <q-icon name="event" class="cursor-pointer">
+                          <q-popup-proxy cover transition-show="scale" transition-hide="scale">
+                            <q-date v-model="dateStart">
+                              <div class="row items-center justify-end">
+                                <q-btn v-close-popup label="Close" color="warning" flat />
+                              </div>
+                            </q-date>
+                          </q-popup-proxy>
+                        </q-icon>
+                      </template>
+                    </q-input>
+                    <div class="DateSpace">-</div>
+                    <q-input filled v-model="dateEnd" mask="date" :rules="['date']" class="DateInput">
+                      <template v-slot:append>
+                        <q-icon name="event" class="cursor-pointer">
+                          <q-popup-proxy cover transition-show="scale" transition-hide="scale">
+                            <q-date v-model="dateEnd">
+                              <div class="row items-center justify-end">
+                                <q-btn v-close-popup label="Close" color="primary" flat />
+                              </div>
+                            </q-date>
+                          </q-popup-proxy>
+                        </q-icon>
+                      </template>
+                    </q-input>
+                  </div>
+                </div>
+              </div>
+            </q-btn-dropdown>
+
+            <q-select color="warning" size="lg" v-model="OrderTypeState" :options="OrderTypeOptions" label="類型" rounded
+              outlined />
+
+            <q-select color="warning" size="lg" v-model="OrderState" :options="OrderStateOptions" label="狀態" rounded
+              outlined />
+
+          </div>
+          <div class="OrderTableBlock q-my-lg">
+            <q-table class="OrderTable" flat :rows="rows" :columns="columns" row-key="name"
+              v-model:pagination="pagination" hide-bottom>
+
+              <template v-slot:no-data="{ icon, message, filter }">
+                <div class="full-width row flex-center text-accent q-gutter-sm">
+                  <q-icon size="2em" name="sentiment_dissatisfied" />
+                  <span>
+                    Well this is sad... {{ message }} {{ filter }}
+                  </span>
+                  <q-icon size="2em" :name="filter ? 'filter_b_and_w' : icon" />
+                </div>
+              </template>
+
+            </q-table>
+            <div class="row justify-center q-mt-md">
+              <q-pagination v-model="pagination.page" :max="pagesNumber" size="sm" direction-links flat color="grey"
+                active-color="warning" gutter="md" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </q-page>
 </template>
 
 <script>
-import { defineComponent } from "vue";
 
-export default defineComponent({
+import { ref, computed } from 'vue'
+import { toThousands } from 'src/utils/index.js'
+
+const columns = [
+  { name: "name", required: true, label: "日期", field: "date", align: 'left' },
+  { name: "calories", label: "類型", field: "type", align: 'center' },
+  { name: "fat", label: "用戶名稱", field: "name", align: 'center' },
+  { name: "carbs", label: "狀態", field: "state", align: 'center' },
+  {
+    name: "protein",
+    label: "手續費",
+    field: "fee",
+    format: (val) => `NTD$ ${toThousands(val)}`,
+    align: 'center'
+  },
+  {
+    name: "sodium",
+    label: "淨額",
+    field: "amt",
+    format: (val) => `NTD$ ${toThousands(val)}`,
+    align: 'right'
+  },
+];
+
+const rows = [
+  {
+    date: "2024/02/06",
+    type: "收款",
+    name: "Digiflow tech.",
+    state: "已付款",
+    fee: 15,
+    amt: 5678,
+  }, {
+    date: "2024/02/06",
+    type: "收款",
+    name: "Digiflow tech.",
+    state: "已提領",
+    fee: 1,
+    amt: 89,
+  }, {
+    date: "2024/02/06",
+    type: "收款",
+    name: "Digiflow tech.",
+    state: "已退款",
+    fee: 5,
+    amt: 100,
+  }, {
+    date: "2024/02/06",
+    type: "收款",
+    name: "Digiflow tech.",
+    state: "已完成",
+    fee: 1,
+    amt: 10,
+  }, {
+    date: "2024/02/06",
+    type: "收款",
+    name: "Digiflow tech.",
+    state: "已完成",
+    fee: 0,
+    amt: 200,
+  }, {
+    date: "2024/02/06",
+    type: "收款",
+    name: "Digiflow tech.",
+    state: "已完成",
+    fee: 0,
+    amt: 200,
+  }, {
+    date: "2024/02/06",
+    type: "收款",
+    name: "Digiflow tech.",
+    state: "已完成",
+    fee: 0,
+    amt: 200,
+  }, {
+    date: "2024/02/06",
+    type: "收款",
+    name: "Digiflow tech.",
+    state: "已完成",
+    fee: 0,
+    amt: 200,
+  }, {
+    date: "2024/02/06",
+    type: "收款",
+    name: "Digiflow tech.",
+    state: "已完成",
+    fee: 0,
+    amt: 200,
+  }, {
+    date: "2024/02/06",
+    type: "收款",
+    name: "Digiflow tech.",
+    state: "已完成",
+    fee: 0,
+    amt: 200,
+  }, {
+    date: "2024/02/06",
+    type: "收款",
+    name: "Digiflow tech.",
+    state: "已完成",
+    fee: 0,
+    amt: 200,
+  }, {
+    date: "2024/02/06",
+    type: "收款",
+    name: "Digiflow tech.",
+    state: "已完成",
+    fee: 0,
+    amt: 200,
+  }, {
+    date: "2024/02/06",
+    type: "收款",
+    name: "Digiflow tech.",
+    state: "已完成",
+    fee: 0,
+    amt: 200,
+  }, {
+    date: "2024/02/06",
+    type: "收款",
+    name: "Digiflow tech.",
+    state: "已完成",
+    fee: 0,
+    amt: 200,
+  },
+];
+
+export default {
   name: "HistoryPage",
-});
+  setup() {
+
+    const pagination = ref({
+      sortBy: 'desc',
+      descending: true,
+      page: 1,
+      rowsPerPage: 10
+    })
+
+    return {
+      text: ref(''),
+      model: ref(null),
+      OrderState: ref(null),
+      OrderTypeState: ref(null),
+      dateGroup: ref(null),
+      dateStart: ref('2019/02/01'),
+      dateEnd: ref('2019/02/01'),
+      OrderStateOptions: [
+        '交易中', '已完成', '已提領', '已退款', '處理中', '已取消'
+      ],
+      OrderTypeOptions: [
+        '付款', '提領', '取消'
+      ],
+      dateOptions: [
+        { label: '本月', value: 'month', color: 'warning' },
+        { label: '上個月', value: 'lastMonth', color: 'warning' },
+        { label: '最近三個月', value: 'season', color: 'warning' },
+        { label: '今年', value: 'year', color: 'warning' },
+        { label: '去年', value: 'lastYear', color: 'warning' }
+      ],
+      columns,
+      rows,
+      pagination,
+      pagesNumber: computed(() => Math.ceil(rows.length / pagination.value.rowsPerPage))
+    }
+  }
+};
 </script>
+
+<style lang="sass">
+
+.filterBlock
+  display: flex
+  flex-wrap: nowrap
+  align-items: center
+  margin: 20px 0
+
+  h5
+    margin: 0 10px 0 0
+
+  button, .q-field
+    min-width: 130px
+    margin: 0 5px
+
+    span
+      color: #333
+
+  .q-field--outlined .q-field__control:before
+    border: 1px solid #fcb335
+  .q-field__label
+    color: #333
+
+.DateRange
+  display: inline-flex
+  align-items: baseline
+
+  .DateSpace
+    width: 20px
+    text-align: center
+
+  .DateInput
+    width: calc(50% - 20px)
+    max-width: 200px
+
+.OrderTable
+  .q-table__top,
+  thead tr:first-child th
+    color: #FFF
+    background-color: #738A94
+
+.q-pagination__middle > .q-btn
+  width: 30px
+  height: 30px
+  border-radius: 50%
+
+.q-btn:before
+  box-shadow: none
+
+
+</style>
--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,53 +1,65 @@
 <template>
-  <q-page class="flex flex-center">
-    <div class="q-pa-md" style="width: 60%">
-      <div class="text-h4">▍登入</div>
-      <br />
-      <div class="text-h6">電子郵件 或 手機號碼</div>
-      <div class="q-gutter-md">
-        <q-input filled v-model="account" label="" stack-label> </q-input>
-        <div class="text-h6">密碼</div>
-        <q-input v-model="password" filled :type="isPwd ? 'password' : 'text'">
-          <template v-slot:append>
-            <q-icon
-              :name="isPwd ? 'visibility_off' : 'visibility'"
-              class="cursor-pointer"
-              @click="isPwd = !isPwd"
-            />
-          </template>
-        </q-input>
-        <q-input label="驗證碼" v-model="verify" stack-label>
-          <template v-slot:control>
-            <div class="self-center full-width no-outline" tabindex="0"></div>
-          </template>
-        </q-input>
-        <q-btn
-          style="width: 100%"
-          color="amber"
-          text-color="black"
-          rounded
-          label="登入"
-          to="/Merchant"
-        />
-        <q-separator />
-        <q-btn
-          style="width: 100%"
-          color="black"
-          text-color="white"
-          rounded
-          label="註冊"
-          to="/Login/Register"
-        />
+  <q-page padding>
+    <div class="LoginContent">
+      <h5 class="MainTitle">登入</h5>
+      <div class="inputContent">
+        <div class="inputGroup">
+          <div class="text-subtitle1">電子郵件 或 手機號碼</div>
+          <q-input color="warning" v-model="account" rounded outlined />
+        </div>
+        <div class="inputGroup">
+          <div class="text-subtitle1">密碼</div>
+          <q-input color="warning" v-model="password" :type="isPwd ? 'password' : 'text'" rounded outlined>
+            <template v-slot:append>
+              <q-icon :name="isPwd ? 'visibility_off' : 'visibility'" class="cursor-pointer" @click="isPwd = !isPwd" />
+            </template>
+          </q-input>
+        </div>
+        <div class="inputGroup VerificationCode">
+          <div class="text-subtitle1 Title">驗證碼</div>
+          <q-input v-model="verify" bottom-slots rounded outlined>
+            <template v-slot:control>
+              <div class="self-center full-width no-outline" tabindex="0"></div>
+            </template>
+            <template v-slot:after>
+              <Sidentify :identifyCode="identifyCode" />
+              <q-icon class="refresh" name="refresh" @click="refreshIdentifyCode" size="md" />
+            </template>
+          </q-input>
+        </div>
       </div>
+      <div class="BtnGroup">
+
+        <q-btn color="warning" text-color="black" label="登入" to="/Merchant" size="lg" rounded />
+        <div class="SeparatorTxt">
+          <span class="line"></span>
+          <span class="text">or</span>
+          <span class="line"></span>
+        </div>
+        <q-btn color="black" text-color="white" label="註冊" to="/Login/Register" size="lg" rounded />
+
+      </div>
+
     </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from "vue";
+
 import { ref } from "vue";
-export default defineComponent({
+import Sidentify from "/src/utils/identify.vue"
+
+export default {
   name: "LoginPage",
+  components: {
+    Sidentify
+  },
+  data() {
+    return {
+      identifyCode: "", //密碼登入圖形驗證碼
+      identifyCodes: "1234567890abcdefghizklmnopqrstuvwxyz", //生成圖形驗證碼依據
+    }
+  },
   setup() {
     return {
       password: ref(""),
@@ -56,5 +68,91 @@ export default defineComponent({
       verify: ref(""),
     };
   },
-});
+  methods: {
+
+    // 刷新验证码
+    refreshIdentifyCode() {
+      this.identifyCode = "";
+      this.makeIdentifyCode(4);
+    },
+    // 生成4位数的随机验证码
+    makeIdentifyCode(l) {
+      for (let i = 0; i < l; i++) {
+        this.identifyCode +=
+          this.identifyCodes[this.randomNum(0, this.identifyCodes.length)];
+      }
+    },
+    // 生成单个验证码
+    randomNum(min, max) {
+      return Math.floor(Math.random() * (max - min) + min);
+    },
+  },
+  mounted() {
+    // 初始化验证码
+    this.identifyCode = "";
+    this.makeIdentifyCode(4);
+  }
+};
 </script>
+
+<style lang="sass">
+.LoginContent
+  max-width: 600px
+  margin: 0 auto
+
+  .MainTitle
+    position: relative
+    display: flex
+    align-items: center
+    margin: 0 0 2rem 0
+    padding-left: 10px
+
+  .MainTitle:before
+    display: inline-block
+    content: ""
+    width: 4px
+    height: 25px
+    background: #ffc107
+    position: absolute
+    left: 0
+
+  .inputContent
+    margin: 30px 0
+
+    .inputGroup
+      margin-bottom: 1rem
+
+.SeparatorTxt
+  width: 100%
+  height: 40px
+  display: flex
+  justify-content: space-between
+  align-items: center
+  margin: 10px auto
+
+  .line
+    height: 2px
+    flex-grow: 1
+    background-color: #dfdfdf
+
+  .text
+    margin: 0 5px
+
+.BtnGroup
+  margin-top: 40px
+
+  .q-btn
+    width: 100%
+
+.VerificationCode
+
+  .Title
+    width: 100%
+
+  .refresh
+    cursor: pointer
+
+  canvas
+    vertical-align: sub
+
+</style>
