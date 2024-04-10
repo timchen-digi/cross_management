1b65541433477a0f6b22d2b822b7f84fe9c55e3f
rtyui01132001 <rtyui01132001@gmail.com>
2024-04-10 11:01:41 +0800

    同步

---
+++ src/components/PayHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-header class="header">
+    <q-toolbar class="Toolbar q-pa-md">
+      <img src="~assets/logos/DigiFlow_logo.svg" class="mainLOGO" />
+    </q-toolbar>
+  </q-header>
+</template>
+
+<script>
+
+export default {
+  name: "HerderComp"
+};
+</script>
+
+<style lang="sass" scoped>
+.header
+  background-color: #f7f7f7
+  border-bottom: 3px solid #ffb300
+
+  .Toolbar
+    max-width: 1200px
+    margin: 0 auto
+
+  .mainLOGO
+    margin: 0 auto
+</style>
