83b93d1baf11c632ea9741e85a2120e7b853e511
DESKTOP-H39554P\Tim <timchen@digiflowtech.com>
2024-11-28 10:44:44 +0800

    交易結果頁面

--- src/pages/Pay/ResultPage.vue
+++ src/pages/Pay/ResultPage.vue
@@ -15,11 +15,17 @@
       </q-item>
     </q-list>
     <div class="BTNGroup">
-      <q-btn color="warning" class="text-subtitle1 q-px-xl" text-color="black" label="關閉視窗" rounded />
+      <q-btn color="warning" class="text-subtitle1 q-px-xl" text-color="black" label="關閉視窗" rounded
+        onclick="window.close()" />
     </div>
   </div>
 </template>
 <script>
+import { ref } from "vue";
+import { useQuasar } from 'quasar'
+import { loadScript } from "vue-plugin-load-script";
+import { pointApi } from 'boot/axios'
+import Swal from 'sweetalert2'
 export default {
   name: "PayResult",
   methods: {},
@@ -28,34 +34,70 @@ export default {
   data() {
   },
   setup() {
+    let uri = window.location.hash.split('?')[1]
+    let params = new URLSearchParams(uri)
+    // MID = params.get("MID")
+    let MID = '332715810000001'
+    let TID = 'Game02'
+    let query = {
+      "param": {
+        "Version": "1.0",
+        "MID": MID,
+        "TID": TID,
+        "TransNO": params.get("TransNO"),
+        "Timestamp": new Date().getTime()
+      }
+    }
+    let Order = [
+      {
+        Title: '特店名稱：',
+        content: "店家名稱",
+      }, {
+        Title: '訂單編號：',
+        content: params.get("TransNO"),
+      }, {
+        Title: '交易金額：',
+        content: "NT$ 30 元",
+      }, {
+        Title: '付款方式：',
+        content: "信用卡",
+      }, {
+        Title: '卡號末四碼：',
+        content: "1007",
+      }
+    ]
+    pointApi.post('/Result', query, {
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+        "isCode": 0,
+        "apiKey": "87654321876543218765432187654321",
+        "type": 0
+      }
+    })
+      .then((response) => {
+        console.log(response.data)
+        if (response.data.param.ReturnCode == "000000") {
+          console.log(response.data.param)
+          this.order = response.data.param.content
+          Swal.fire(this.order);
+        }
+        else {
+          alert("查詢失敗，錯誤代碼" + response.data.param.ReturnCode)
+          $q.notify({
+            type: 'negative',
+            message: response.data.param.ReturnMsg,
+            position: "center",
+          });
+        }
+      })
+      .catch(function (error) {
+        console.log("查詢失敗")
+      })
     return {
-      Title: "%這裡放店家名稱%",
+      Title: "數位鎏線上付款",
       OrderStatus: true,
       OrderStatusText: "交易成功",
-      Order: [
-        {
-          Title: '特店名稱：',
-          content: "店家名稱",
-        }, {
-          Title: '消費者姓名：',
-          content: "訂購人名稱",
-        }, {
-          Title: '消費者電話：',
-          content: "0988777666",
-        }, {
-          Title: '訂單編號：',
-          content: "POS.88555462",
-        }, {
-          Title: '交易金額：',
-          content: "NT$ 5 元",
-        }, {
-          Title: '付款方式：',
-          content: "信用卡",
-        }, {
-          Title: '卡號末四碼：',
-          content: "2752",
-        }
-      ]
+      Order
     }
   },
 };
